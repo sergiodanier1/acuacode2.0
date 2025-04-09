2452dfaf3dec60f1c7215e153c6f3275ea7e9575
sergiodanier <sergiodanier@gmail.com>
2025-04-09 18:09:50 -0500

    Actualización del sidebar

--- resources/views/plantilla/sidebar.blade.php
+++ resources/views/plantilla/sidebar.blade.php
@@ -93,275 +93,35 @@
 
     <li>
         <a href="javascript: void(0);">
-            <i class="la la-diamond"></i>
-            <span class="badge badge-danger float-right">New</span>
-            <span> Extra Pages </span>
-        </a>
-        <ul class="nav-second-level" aria-expanded="false">
-            <li>
-                <a href="extras-profile.html">Profile</a>
-            </li>
-            <li>
-                <a href="extras-timeline.html">Timeline</a>
-            </li>
-            <li>
-                <a href="extras-invoice.html">Invoice</a>
-            </li>
-            <li>
-                <a href="extras-faqs.html">FAQs</a>
-            </li>
-            <li>
-                <a href="extras-pricing.html">Pricing</a>
-            </li>
-            <li>
-                <a href="extras-maintenance.html">Maintenance</a>
-            </li>
-            <li>
-                <a href="extras-coming-soon.html">Coming Soon</a>
-            </li>
-        </ul>
-    </li>
-
-    <li class="menu-title mt-2">Components</li>
-
-    <li>
-        <a href="javascript: void(0);">
-            <i class="la la-briefcase"></i>
-            <span> UI Elements </span>
+            <i class="la la-file-text-o"></i>
+            <span> Gestión de Sistemas </span>
             <span class="menu-arrow"></span>
         </a>
         <ul class="nav-second-level" aria-expanded="false">
             <li>
-                <a href="ui-buttons.html">Buttons</a>
+                <a href="pages-starter.html">Sistemas registrados</a>
             </li>
             <li>
-                <a href="ui-cards.html">Cards</a>
-            </li>
-            <li>
-                <a href="ui-tabs-accordions.html">Tabs & Accordions</a>
-            </li>
-            <li>
-                <a href="ui-modals.html">Modals</a>
-            </li>
-            <li>
-                <a href="ui-progress.html">Progress</a>
-            </li>
-            <li>
-                <a href="ui-notifications.html">Notifications</a>
-            </li>
-            <li>
-                <a href="ui-general.html">General UI</a>
-            </li>
-            <li>
-                <a href="ui-typography.html">Typography</a>
-            </li>
-            <li>
-                <a href="ui-grid.html">Grid</a>
-            </li>
+                <a href="pages-login.html">Editar sistemas</a>
+            </li>           
         </ul>
     </li>
-
-    <li>
-        <a href="widgets.html">
-            <i class="la la-coffee"></i>
-            <span> Widgets </span>
-        </a>
-    </li>
-
     <li>
         <a href="javascript: void(0);">
-            <i class="la la-bullhorn"></i>
-            <span class="badge badge-info float-right">Hot</span>
-            <span> Admin UI </span>
-        </a>
-        <ul class="nav-second-level" aria-expanded="false">
-            <li>
-                <a href="admin-sweet-alert.html">Sweet Alert</a>
-            </li>
-            <li>
-                <a href="admin-nestable.html">Nestable List</a>
-            </li>
-            <li>
-                <a href="admin-range-slider.html">Range Slider</a>
-            </li>
-            <li>
-                <a href="admin-tour.html">Tour Page</a>
-            </li>
-            <li>
-                <a href="admin-lightbox.html">Lightbox</a>
-            </li>
-            <li>
-                <a href="admin-treeview.html">Treeview</a>
-            </li>
-        </ul>
-    </li>
-
-    <li>
-        <a href="javascript: void(0);">
-            <i class="la la-bullseye"></i>
-            <span> Icons </span>
+            <i class="la la-file-text-o"></i>
+            <span> Configuracion </span>
             <span class="menu-arrow"></span>
         </a>
         <ul class="nav-second-level" aria-expanded="false">
             <li>
-                <a href="icons-feather.html">Feather Icons</a>
+                <a href="pages-starter.html">Umbrales de sensores</a>
             </li>
             <li>
-                <a href="icons-lineawesome.html">Line Awesome</a>
+                <a href="pages-login.html">Intervalo de muestreo</a>
             </li>
             <li>
-                <a href="icons-mdi.html">Material Design Icons</a>
-            </li>
-            <li>
-                <a href="icons-font-awesome.html">Font Awesome</a>
-            </li>
-            <li>
-                <a href="icons-simple-line.html">Simple Line</a>
-            </li>
-        </ul>
-    </li>
-
-    <li>
-        <a href="javascript: void(0);">
-            <i class="la la-wrench"></i>
-            <span> Forms </span>
-            <span class="menu-arrow"></span>
-        </a>
-        <ul class="nav-second-level" aria-expanded="false">
-            <li>
-                <a href="forms-elements.html">General Elements</a>
-            </li>
-            <li>
-                <a href="forms-advanced.html">Advanced</a>
-            </li>
-            <li>
-                <a href="forms-validation.html">Validation</a>
-            </li>
-            <li>
-                <a href="forms-pickers.html">Pickers</a>
-            </li>
-            <li>
-                <a href="forms-wizard.html">Wizard</a>
-            </li>
-            <li>
-                <a href="forms-masks.html">Masks</a>
-            </li>
-            <li>
-                <a href="forms-summernote.html">Summernote</a>
-            </li>
-            <li>
-                <a href="forms-quilljs.html">Quilljs Editor</a>
-            </li>
-            <li>
-                <a href="forms-file-uploads.html">File Uploads</a>
-            </li>
-        </ul>
-    </li>
-
-    <li>
-        <a href="javascript: void(0);">
-            <i class="la la-table"></i>
-            <span> Tables </span>
-            <span class="menu-arrow"></span>
-        </a>
-        <ul class="nav-second-level" aria-expanded="false">
-            <li>
-                <a href="tables-basic.html">Basic Tables</a>
-            </li>
-            <li>
-                <a href="tables-datatables.html">Data Tables</a>
-            </li>
-            <li>
-                <a href="tables-editable.html">Editable Tables</a>
-            </li>
-            <li>
-                <a href="tables-responsive.html">Responsive Tables</a>
-            </li>
-            <li>
-                <a href="tables-tablesaw.html">Tablesaw Tables</a>
-            </li>
-        </ul>
-    </li>
-
-    <li>
-        <a href="javascript: void(0);">
-            <i class="la la-bar-chart"></i>
-            <span> Charts </span>
-            <span class="menu-arrow"></span>
-        </a>
-        <ul class="nav-second-level" aria-expanded="false">
-            <li>
-                <a href="charts-apex.html">Apex Charts</a>
-            </li>
-            <li>
-                <a href="charts-flot.html">Flot Charts</a>
-            </li>
-            <li>
-                <a href="charts-morris.html">Morris Charts</a>
-            </li>
-            <li>
-                <a href="charts-chartjs.html">Chartjs Charts</a>
-            </li>
-            <li>
-                <a href="charts-bright.html">Bright Charts</a>
-            </li>
-            <li>
-                <a href="charts-chartist.html">Chartist Charts</a>
-            </li>
-            <li>
-                <a href="charts-peity.html">Peity Charts</a>
-            </li>
-            <li>
-                <a href="charts-sparklines.html">Sparklines Charts</a>
-            </li>
-            <li>
-                <a href="charts-knob.html">Jquery Knob Charts</a>
-            </li>
-        </ul>
-    </li>
-
-    <li>
-        <a href="javascript: void(0);">
-            <i class="la la-map"></i>
-            <span> Maps </span>
-            <span class="menu-arrow"></span>
-        </a>
-        <ul class="nav-second-level" aria-expanded="false">
-            <li>
-                <a href="maps-google.html">Google Maps</a>
-            </li>
-            <li>
-                <a href="maps-vector.html">Vector Maps</a>
-            </li>
-            <li>
-                <a href="maps-mapael.html">Mapael Maps</a>
-            </li>
-        </ul>
-    </li>
-
-    <li>
-        <a href="javascript: void(0);">
-            <i class="la la-plus-square-o"></i>
-            <span> Multi Level </span>
-            <span class="menu-arrow"></span>
-        </a>
-        <ul class="nav-second-level nav" aria-expanded="false">
-            <li>
-                <a href="javascript: void(0);">Level 1.1</a>
-            </li>
-            <li>
-                <a href="javascript: void(0);" aria-expanded="false">Level 1.2
-                    <span class="menu-arrow"></span>
-                </a>
-                <ul class="nav-third-level nav" aria-expanded="false">
-                    <li>
-                        <a href="javascript: void(0);">Level 2.1</a>
-                    </li>
-                    <li>
-                        <a href="javascript: void(0);">Level 2.2</a>
-                    </li>
-                </ul>
-            </li>
+                <a href="pages-register.html">Parametros de control</a>
+            </li>           
         </ul>
     </li>
 </ul>
